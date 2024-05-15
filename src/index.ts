42edd5ad24584373f7086adb40e044ac9d1d2653
mauromamani <mauromamani.b@gmail.com>
2024-05-14 21:58:29 -0300

    create worker

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,113 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler deploy src/index.ts --name my-worker` to deploy your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { env } from 'hono/adapter';
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-
+  [key: string]: unknown;
   DKIM_PRIVATE_KEY: string;
 }
 
-export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    if (request.method !== 'POST') {
-      return new Response('Method Not Allowed', { status: 405 });
-    }
+interface IBody {
+  to: {
+    email: string;
+    name: string;
+  };
+  from: {
+    email: string;
+    name: string;
+  };
+  subject: string;
+  content: {
+    type: string;
+    value: string;
+  }[];
+}
 
-    const recipient = 'mauromamani.b@gmail.com';
+const app = new Hono();
 
-    const body = {
-      personalizations: [
-        {
-          to: [{ email: recipient, name: 'asd' }],
-          dkim_domain: 'mauromamani.store',
-          dkim_selector: 'mailchannels',
-          dkim_private_key: env.DKIM_PRIVATE_KEY,
-        },
-      ],
-      from: {
-        email: 'mauro@gmail.com',
-        name: 'asd',
+app.use('/*', cors());
+
+app.post('/api/send-email', async (ctx) => {
+  const body: IBody = await ctx.req.json();
+
+  const { DKIM_PRIVATE_KEY } = env<Env>(ctx);
+
+  const bodyToSend = {
+    personalizations: [
+      {
+        to: [{ email: body.to.email, name: body.to.name }],
+        dkim_domain: 'mauromamani.store',
+        dkim_selector: 'mailchannels',
+        dkim_private_key: DKIM_PRIVATE_KEY,
       },
-      subject: 'Look! No servers',
-      content: [
-        {
-          type: 'text/plain',
-          value: 'And no email service accounts and all for free too!',
-        },
-      ],
-    };
+    ],
+    from: {
+      email: body.from.email,
+      name: body.from.name,
+    },
+    subject: body.subject,
+    content: body.content,
+  };
 
-    const response = await fetch('https://api.mailchannels.net/tx/v1/send', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
+  const response = await fetch('https://api.mailchannels.net/tx/v1/send', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(bodyToSend),
+  });
 
-    const respText = await response.text();
+  const respText = await response.text();
 
-    return new Response(respText || 'ok');
-  },
-};
+  return new Response(respText || 'ok');
+});
+
+export default app;
+
+// export default {
+//   async fetch(
+//     request: Request,
+//     env: Env,
+//     ctx: ExecutionContext
+//   ): Promise<Response> {
+//     if (request.method !== 'POST') {
+//       return new Response('Method Not Allowed', { status: 405 });
+//     }
+
+//     const recipient = 'mauromamani.b@gmail.com';
+
+//     const body = {
+//       personalizations: [
+//         {
+//           to: [{ email: recipient, name: 'mauromamani' }],
+//           dkim_domain: 'mauromamani.store',
+//           dkim_selector: 'mailchannels',
+//           dkim_private_key: env.DKIM_PRIVATE_KEY,
+//         },
+//       ],
+//       from: {
+//         email: 'info@mauromamani.store',
+//         name: 'INFO',
+//       },
+//       subject: 'Look! No servers',
+//       content: [
+//         {
+//           type: 'text/plain',
+//           value: 'And no email service accounts and all for free too!',
+//         },
+//       ],
+//     };
+
+//     const response = await fetch('https://api.mailchannels.net/tx/v1/send', {
+//       method: 'POST',
+//       headers: {
+//         'content-type': 'application/json',
+//       },
+//       body: JSON.stringify(body),
+//     });
+
+//     const respText = await response.text();
+
+//     return new Response(respText || 'ok');
+//   },
+// };
